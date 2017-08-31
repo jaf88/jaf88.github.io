6f9640fb4c84cab6fa053f8bbf16478c9866c5a1
jaf88 <jaf.alves8@gmail.com>
2017-08-30 21:20:18 -0300

    Questão 04 - Exercícios JS

--- aulaExerciciosJS/js/js.js
+++ aulaExerciciosJS/js/js.js
@@ -29,6 +29,7 @@ Faça um algoritmo que leia dois valores inteiros A e B se os valores forem igua
 ser somado os dois, caso contrário multiplique A por B, ao final do cálculo ao final do
 cálculo atribuir o valor para uma variável C.
 */
+
 function questao3() {
   var a = prompt("Informe um valor para A!");
   var b = prompt("Informe um valor para B!");
@@ -43,6 +44,31 @@ function questao3() {
   } else {
     c = a * b;
     alert("A e B são diferentes e C é igual a: "+(c));
+  }
+
+}
+
+/*
+A partir do preço à vista de um determinado produto, calcule o preço total a pagar e o
+valor da prestação mensal, referentes ao pagamento parcelado. Se o pagamento for
+parcelado em 3 vezes deve ser dado um acréscimo de 10% no total a ser pago. Se o
+parcelamento for em 5 vezes, o acréscimo será de 20%.
+*/
+function questao4() {
+  var produto = prompt("Informe o valor do produto à vista!");
+  var parcelas = prompt("Informe o número de parcelas que deseja!");
+
+  produto = parseFloat(produto);
+  parcelas = parseInt(parcelas);
+
+  if(parcelas <= 3) {
+    produto = produto+(produto*0.10);
+    parcelas = produto/parcelas;
+    alert('O valor da prestação fica em: '+(parcelas) + ' e o valor total é: '+(produto));
+  } else if(parcelas >= 5){
+    produto = produto+(produto*0.20);
+    parcelas = produto/parcelas;
+    alert('O valor da prestação fica em: '+(parcelas) + ' e o valor total é: '+(produto));
   }
 
 }
