fdfdf522dcdeb75020b704d41df19d0e58b39691
jaf88 <jaf.alves8@gmail.com>
2017-09-06 21:04:00 -0300

    Jogo da Velha - Começo - 2

--- jogo_da_veia/jogo.js
+++ jogo_da_veia/jogo.js
@@ -2,5 +2,7 @@
 $(document).ready(function() {
   $('#btn_iniciar_jogo').click(function () {
     alert('Jogo iniciado!');
+    alert($('#entrada_play_1').val());
+    alert($('#entrada_play_2').val());
   });
 });
