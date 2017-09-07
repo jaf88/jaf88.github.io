490802a721c162ccd5e9355c7c84aa06c3226e0b
jaf88 <jaf.alves8@gmail.com>
2017-09-06 21:10:45 -0300

    Eventos de Click Jogo da Veia

--- jogo_da_veia/jogo.js
+++ jogo_da_veia/jogo.js
@@ -1,7 +1,18 @@
 
 $(document).ready(function() {
+
   $('#btn_iniciar_jogo').click(function () {
-    alert('Jogo iniciado!');
+
+    if ($('#entrada_play_1').val()=='') {
+      alert("O apelido do jogador 1 deve ser informado!");
+      return false;
+    }
+
+    if ($('#entrada_play_2').val()=='') {
+      alert("O apelido do jogador 2 deve ser informado!");
+      return false;
+    }
+    //alert('Jogo iniciado!');
     alert($('#entrada_play_1').val());
     alert($('#entrada_play_2').val());
   });
