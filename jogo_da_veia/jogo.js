24bfca5e183df0ca3e65bdd15ed5a015b6ba43d0
jaf88 <jaf.alves8@gmail.com>
2017-09-10 15:05:19 -0300

    Parte 1 ok - Jogo da Veia

--- jogo_da_veia/jogo.js
+++ jogo_da_veia/jogo.js
@@ -16,8 +16,15 @@ $(document).ready(function() {
     $('#name_play_1').html($('#entrada_play_1').val());
     $('#name_play_2').html($('#entrada_play_2').val());
 
+    $('#pagina_inicial').hide();
+    $('#palco_jogo').show();
+
     //alert('Jogo iniciado!');
     //alert($('#entrada_play_1').val());
     //alert($('#entrada_play_2').val());
   });
+
+  $('.jogada').click(function(){
+    alert("jogada capturada");
+  })
 });
