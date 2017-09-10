e5b4796e1d8c89a2c9e10443d2d128e529158488
jaf88 <jaf.alves8@gmail.com>
2017-09-10 13:05:04 -0300

    jogo da veia - span dos nomes ok

--- jogo_da_veia/jogo.js
+++ jogo_da_veia/jogo.js
@@ -9,11 +9,15 @@ $(document).ready(function() {
     }
 
     if ($('#entrada_play_2').val()=='') {
-      alert("O apelido do jogador 2 deve ser informado!");
+      alert("O apelido do jogador 2  deve ser informado!");
       return false;
     }
+
+    $('#name_play_1').html($('#entrada_play_1').val());
+    $('#name_play_2').html($('#entrada_play_2').val());
+
     //alert('Jogo iniciado!');
-    alert($('#entrada_play_1').val());
-    alert($('#entrada_play_2').val());
+    //alert($('#entrada_play_1').val());
+    //alert($('#entrada_play_2').val());
   });
 });
