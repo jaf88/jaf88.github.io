711612ccd68d509b2a155a4f77a37859f3033c87
jaf88 <jaf.alves8@gmail.com>
2017-09-11 09:35:52 -0300

    Ajustes

--- aula26/js.js
+++ aula26/js.js
@@ -35,4 +35,28 @@ $(document).ready(function(){
   $('.caixamo').mouseover(function(){
       alert('Evento Mouse Over');
   });
+
+  $(".campo").keypress(function() {
+    $(this).css("background-color", "#cccccc");
+  });
+
+  $(".input_keyup").keyup(function(){
+    $(".input_keyup").css("background-color", "red");
+  });
+
+  $(".campo").focus(function(){
+    $(this).css("background-color", "yellow");
+  });
+
+  $(".campo").blur(function(){
+    $(this).css("background-color", "#ffffff");
+  });
+
+  $(".combo").change(function(){
+    alert('O Elemento foi modificado: Pesquisar cidades do '+$(this).val());
+  });
+
+  $(".combo_site").change(function(){
+    alert('O Elemento foi modificado: Pesquisar cidades do '+$(this).val());
+  });
 });
